e8ce3435a3cf95096abc215795e86ee1ceaf03cb
Romain Vincent <romainfvincent@gmail.com>
2017-05-14 22:53:41 +0200

    minor changes

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -12,187 +12,143 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryController extends Controller
 {
-    public function indexAction(Request $request)
-    {
-        $categories = $this
-            ->getDoctrine()
-            ->getRepository('AppBundle:Category')
-            ->findAll();
-
-        return $this->render('AppBundle:Category:index.html.twig', array(
-            'categories' => $categories
-        ));
-    }
-
-public function createAction(Request $request)
-
+  public function indexAction(Request $request)
   {
+    // Get the categories
+    $categories = $this
+      ->getDoctrine()
+      ->getRepository('AppBundle:Category')
+      ->findAll();
 
-    // On crée un objet Category
+    return $this->render('AppBundle:Category:index.html.twig', array(
+      'categories' => $categories
+    ));
+  }
 
+  public function createAction(Request $request)
+  {
     $category = new Category();
 
+    // Build the form
+    $form = $this
+      ->createFormBuilder($category)
+      ->add('labelEn', TextType::class, array('required' => false))
+      ->add('labelFr', TextType::class, array('required' => false))
+      ->add('labelDe', TextType::class, array('required' => false))
+      ->add('save', SubmitType::class)
+      ->getForm();
 
-  
+    // Handle user input
+	  $form->handleRequest($request);
 
-    $form = $this->createFormBuilder($category)
-
-
-
-      ->add('labelEn',     TextType::class, array('required' => false))
-
-      ->add('labelFr',     TextType::class, array('required' => false))
-
-      ->add('labelDe',     TextType::class, array('required' => false))
-
-      ->add('save',       SubmitType::class)
-    
-            ->getForm();
-
-
-	$form->handleRequest($request);
-
-
-    // On vérifie que les valeurs entrées sont correctes
-
-    // (Nous verrons la validation des objets en détail dans le prochain chapitre)
-
+    // If no errors
     if ($form->isValid()) {
-
-      // On l'enregistre notre objet $category dans la base de données, par exemple
-
+      //shortcut
       $em = $this->getDoctrine()->getManager();
-
+      // Update the database
       $em->persist($category);
-
       $em->flush();
 
+      // Notify the view
+      $request
+        ->getSession()
+        ->getFlashBag()
+        ->add('notice', 'Category registered successfully.');
 
-      $request->getSession()->getFlashBag()->add('notice', 'Catégorie enregistrée.');
-
-
-      // On redirige vers la page de visualisation de l'annonce nouvellement créée
-
+      // Redirect
       return $this->redirect($this->generateUrl('app_categories'));
-
     }
-
-        return $this->render('AppBundle:Category:create.html.twig', array(
-            'form' => $form->createView(),
-        ));
-
+    // Render the form
+    return $this->render('AppBundle:Category:create.html.twig', array(
+        'form' => $form->createView(),
+    ));
   }
 
-
- public function deleteAction($id, Request $request)
-
+  public function deleteAction($id, Request $request)
   {
-
+    // shortcut
     $em = $this->getDoctrine()->getManager();
 
+    // Get Category
+    $category = $em
+      ->getRepository('AppBundle:Category')
+      ->find($id);
 
-    // On récupère l'annonce $id
-
-    $category = $em->getRepository('AppBundle:Category')->find($id);
-
-
+    // Handle null category
     if (null === $category) {
-
       throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
-
     }
 
+    // Build the form
+    $form = $this
+      ->createFormBuilder()
+      ->getForm();
 
-    // On crée un formulaire vide, qui ne contiendra que le champ CSRF
-
-    // Cela permet de protéger la suppression d'annonce contre cette faille
-
-    $form = $this->createFormBuilder()->getForm();
-
-
+    // If no errors
     if ($form->handleRequest($request)->isValid()) {
-
+      // Update database
       $em->remove($category);
-
       $em->flush();
 
-
-      $request->getSession()->getFlashBag()->add('info', "L'annonce a bien été supprimée.");
-
-
+      // Notify the view
+      $request
+        ->getSession()
+        ->getFlashBag()
+        ->add('info', "Category deleted successfully.");
+      
+      // Redirect
       return $this->redirect($this->generateUrl('app_categories'));
-
     }
 
-
-    // Si la requête est en GET, on affiche une page de confirmation avant de supprimer
-
+    // Render delete confirmation page
     return $this->render('AppBundle:Category:delete.html.twig', array(
-
       'category' => $category,
-
       'form'   => $form->createView()
-
     ));
-
   }
 
-public function changeAction($id, Request $request)
-
+  public function changeAction($id, Request $request)
   {
-
+    // Shortcut
     $em = $this->getDoctrine()->getManager();
 
+    // Get Category
+    $category = $em
+      ->getRepository('AppBundle:Category')
+      ->find($id);
 
-    // On récupère l'annonce $id
-
-    $category = $em->getRepository('AppBundle:Category')->find($id);
-
-
+    // Handle null category
     if (null === $category) {
-
-      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
-
+      throw new NotFoundHttpException("No category with id=".$id." were found.");
     }
 
+    // Build the form
+    $form = $this->createFormBuilder($category)
+      ->add('labelEn', TextType::class, array('required' => false))
+      ->add('labelFr', TextType::class, array('required' => false))
+      ->add('labelDe', TextType::class, array('required' => false))
+      ->add('save', SubmitType::class)
+      ->getForm();
 
-   $form = $this->createFormBuilder($category)
-
-
-
-      ->add('labelEn',     TextType::class, array('required' => false))
-
-      ->add('labelFr',     TextType::class, array('required' => false))
-
-      ->add('labelDe',     TextType::class, array('required' => false))
-
-      ->add('save',       SubmitType::class)
-    
-            ->getForm();
-
-
+    // If no errors
     if ($form->handleRequest($request)->isValid()) {
-
-      // Inutile de persister ici, Doctrine connait déjà notre annonce
-
+      // Update database
       $em->flush();
-
-
-      $request->getSession()->getFlashBag()->add('notice', 'Catégorie modifiée.');
-
-
-      return $this->redirect($this->generateUrl('app_categories', array('id' => $category->getId())));
-
+      // notify on view
+      $request
+        ->getSession()
+        ->getFlashBag()
+        ->add('notice', 'Category changed successfully.');
+      // redirect
+      return $this->redirect($this->generateUrl('app_categories', array(
+        'id' => $category->getId()
+      )));
     }
 
-
+    // Else: Render a new form
     return $this->render('AppBundle:Category:change.html.twig', array(
-
       'form'   => $form->createView(),
-
-      'Category' => $category // Je passe également l'annonce à la vue si jamais elle veut l'afficher
-
+      'category' => $category
     ));
-
   }
 }
